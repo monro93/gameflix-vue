0a20864d84c0185b7218960d6bffee18fbd2ee12
Albert Montolio <albert.montolio@glovoapp.com>
2023-03-31 18:16:02 +0200

    Add Index page with the MainGame component

---
+++ src/components/MainGame.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import {Game} from "@/types/Game.types";
+
+const game = new Game('Half life Alyx', 'co1tq3')
+
+</script>
+
+<template>
+  <div class="main-game-div-full-page" :style="{backgroundImage: game.imageBackground()}">
+    <h1 class="name">{{game.name}}</h1>
+    <div class="rating"></div>
+    <p class="description">{{game.name}}</p>
+  </div>
+</template>
+
+
+<style scoped>
+.main-game-div-full-page {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MainGame from "@/components/MainGame.vue";
+</script>
+
+<template>
+  <MainGame/>
+</template>
+
+<style scoped>
+
+</style>
