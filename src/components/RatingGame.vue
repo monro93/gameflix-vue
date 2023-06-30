8e70674591c5af9b305aae2397bfb777e5fd4a55
Albert Montolio <albert.montolio@glovoapp.com>
2023-06-30 16:46:05 +0200

    Improve rating component

--- src/components/RatingGame.vue
+++ src/components/RatingGame.vue
@@ -1,40 +1,52 @@
 <script setup lang="ts">
-defineProps({ value: Number })
+defineProps({value: Number})
 </script>
 
 <template>
-  <div class="star">
-    <div class="star-fill" :style="{ height: value + '%' }" :data-value="value"></div>
-    <div class="star-grey"></div>
-  </div>
+    <div class="star star-form">
+        <div class="star-fill" :style="{ height: value + '%' }" :data-value="value"></div>
+        <div class="star-grey"></div>
+        <p class="rating-text">{{(value/10).toFixed(1)}}</p>
+    </div>
 </template>
 
 <style scoped>
+.star-form  {
+    clip-path: polygon(50% 0%, 33% 33%, 0% 38%, 23% 66%, 17% 100%, 50% 83%, 83% 100%, 77% 66%, 100% 38%, 66% 33%);
+}
 .star {
-  position: relative;
-  height: 100px;
-  width: 100px;
-  clip-path: polygon(50% 2.45%, 61.8% 38.77%, 100% 38.77%, 69.1% 61.23%, 80.9% 97.55%, 50% 75.1%, 19.1% 97.55%, 30.9% 61.23%, 0% 38.77%, 38.2% 38.77%);
+    position: relative;
+    height: 100px;
+    width: 100px;
 }
 
 .star-fill {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  background-color: yellow;
-  height: 0;
-  width: 100%;
-  z-index: 10;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: yellow;
+    height: 0;
+    width: 100%;
+    z-index: 10;
 }
 
 .star-grey {
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: grey;
-  height: 100%;
-  width: 100%;
-  clip-path: polygon(50% 2.45%, 61.8% 38.77%, 100% 38.77%, 69.1% 61.23%, 80.9% 97.55%, 50% 75.1%, 19.1% 97.55%, 30.9% 61.23%, 0% 38.77%, 38.2% 38.77%);
-  mix-blend-mode: difference;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: grey;
+    height: 100%;
+    width: 100%;
+}
+
+.rating-text {
+    position: absolute;
+    top: 45%;
+    text-align: center;
+    line-height: 100%;
+    font-size: 1.5em;
+    width: 100%;
+    color: black;
+    z-index: 100;
 }
 </style>
